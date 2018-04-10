b7be77be26c1c7e377178ed3ef4323bc89f8dce9
antoinelemarie <antoine.lemarie.13480@gmail.com>
2018-04-10 11:47:09 +0200

    ??

--- src/lsg/LearningSoulsGame.java
+++ src/lsg/LearningSoulsGame.java
@@ -14,6 +14,7 @@ import lsg.weapons.Mass;
 import lsg.weapons.Spears;
 import lsg.weapons.Swords;
 import lsg.weapons.Weapons;
+import util.Scanner;
 
 /**
  * @author antoinelemarie
@@ -22,7 +23,9 @@ import lsg.weapons.Weapons;
  *
  */
 public class LearningSoulsGame {
-
+	Hero hero;
+	Monsters monster;
+	Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
@@ -31,73 +34,47 @@ public class LearningSoulsGame {
 		
 		Monsters monster1 = new Monsters(100, 200);
 		System.out.println(monster1.printStats());
-//		
-//		Monsters monster2 = new Monsters();
-//		System.out.println(monster2.printStats());
-//		
-//		Monsters monster3 = new Monsters(0,10);
-//		System.out.println(monster3.printStats());
-//		
-//		Monsters boss = new Monsters("boss", 30, 40);
-//		System.out.println(boss.printStats());
-//		
-//		Monsters etudiator = new Monsters("etudiator");
-//		System.out.println(etudiator.printStats());
-//		
-//		Pnjs bobby = new Pnjs("Bobbyyyy!");
-//		System.out.println(bobby.printStats());
-//		
-//		Sellers marchand = new Sellers("Marchand");
-//		System.out.println(marchand.printStats());
-//		
-//		Weapons wooden_staff = new Weapons();
-//		System.out.println(wooden_staff.printStats());
-//		
+		
+		Swords new_sword = new Swords("Gilbert Berangeru nasty sauce du granu jnounu de trois semainu de feu", 999999999, 999999999, 999999999);
+		System.out.println(new_sword.printStats());
+
 		Swords sword = new Swords(60, 210, 210);
-		System.out.println(sword.printStats());
+		//System.out.println(sword.printStats());
 		
 		Swords branch = new Swords();
-		System.out.println(branch.printStats());
-//		
-//		Spears spear = new Spears();
-//		System.out.println(spear.printStats());
-//		
-//		Axes axe = new Axes();
-//		System.out.println(axe.printStats());
-//		
-//		Mass mass = new Mass();
-//		System.out.println(mass.printStats());
+		//System.out.println(branch.printStats());
+;
 		
 		hero.setArme(sword);
 		monster1.setArme(branch);
 		
 		hero.Attack();
 		
-		System.out.println("1 "+monster1.printStats());
+		//System.out.println("1 "+monster1.printStats());
 		
 		monster1.Attack();
 		
-		System.out.println("2 "+hero.printStats());
+		//System.out.println("2 "+hero.printStats());
 		
 		hero.Attack();
 		
-		System.out.println("3 "+monster1.printStats());
+		//System.out.println("3 "+monster1.printStats());
 		
 		hero.Attack();
 		
-		System.out.println("4 "+monster1.printStats());
+		//System.out.println("4 "+monster1.printStats());
 		
 		monster1.Attack();
 		
-		System.out.println("5 "+hero.printStats());
+		//System.out.println("5 "+hero.printStats());
 		
 		hero.Attack();
 		
-		System.out.println("5 "+hero.printStats());
+		//System.out.println("5 "+hero.printStats());
 
 		hero.Attack();
 		
-		System.out.println("5 "+hero.printStats());
+		//System.out.println("5 "+hero.printStats());
 		
 		
 		
--- src/util/Scanner.java
+++ src/util/Scanner.java
@@ -3,16 +3,27 @@
  */
 package util;
 
+import java.io.InputStream;
+
+import lsg.characters.Hero;
+import lsg.characters.Monsters;
+
 /**
  * @author antoinelemarie
  *
  */
 public class Scanner {
 
+	Hero hero;
+	Monsters monster;
+	Scanner scanner = new Scanner(System.in);
 	/**
 	 * 
 	 */
 	public Scanner() {
+		// TODO Auto-generated constructor stub
+	}
+	public Scanner(InputStream in) {
 		// TODO Auto-generated constructor stub
 	}
 
