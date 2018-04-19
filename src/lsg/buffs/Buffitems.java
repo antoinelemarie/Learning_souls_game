9d0fd6ddbc9264996553b6a5751ea125b69c0756
antoinelemarie <antoine.lemarie.13480@gmail.com>
2018-04-19 23:20:44 +0200

    creation buffitem

---
+++ src/lsg/buffs/Buffitems.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package lsg.buffs;
+
+/**
+ * @author antoinelemarie
+ *
+ */
+public abstract class Buffitems {
+
+	/**
+	 * 
+	 */
+	public Buffitems() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
